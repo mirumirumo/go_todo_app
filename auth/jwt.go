d0a95c27102cbc5b98f209a67eb3da3e0a8f2694
mirumirumo <yoshinouchi.m.aa@gmail.com>
2024-09-21 16:18:46 +0900

    interfaceのエラー修正

--- auth/jwt.go
+++ auth/jwt.go
@@ -33,6 +33,7 @@ type JWTer struct {
 }
 
 //go:generate go run github.com/matryer/moq -out moq_test.go . Store
+type Store interface {
 	Save(ctx context.Context, key string, userID entity.UserID) error
 	Load(ctx context.Context, key string) (entity.UserID, error)
 }
